d539c30b678fec00c548661bb258945e4e9c0aa3
av3000 <alanas.vaiciulis@gmail.com>
2025-03-14 00:23:04 +0100

    feat: install vite, move files according to vite structure

---
+++ client/src/main.jsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+import ReactDOM from "react-dom";
+import "./assets/font-awesome/css/all.min.css";
+import "./styles/index.scss";
+import App from "./containers/app/App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
