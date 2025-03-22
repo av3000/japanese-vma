7c046d9565e4a29e07171c3e819a5bb4f2f49427
av3000 <alanas.vaiciulis@gmail.com>
2025-03-22 23:54:42 +0100

    feat: create link component and icon

---
+++ client/src/assets/icons/index.tsx
@@ -0,0 +1,13 @@
+import fbIcon from "./fb-icon.svg?raw";
+import igIcon from "./ig-icon.svg?raw";
+import chevron from "./chevron.svg?raw";
+import user from "./user.svg?raw";
+import plus from "./plus.svg?raw";
+
+export const icons = {
+  plus,
+  user,
+  fbIcon,
+  igIcon,
+  chevron,
+} as const;
---
+++ client/src/components/shared/Link/types.ts
@@ -0,0 +1,19 @@
+export type LinkColor = "default";
+
+export const linkSizes = ["sm", "md", "lg"] as const;
+
+export type LinkSize = (typeof linkSizes)[number];
+
+export const linkWeights = ["regular", "bold"] as const;
+
+export type LinkWeightType = (typeof linkWeights)[number];
+
+export interface LinkBaseProps {
+  readonly textLink?: boolean;
+  readonly richTextLink?: boolean;
+  readonly color?: LinkColor;
+  readonly size?: LinkSize;
+  readonly weight?: LinkWeightType;
+  readonly hideExternalIcon?: boolean;
+  readonly hasMinHeight?: boolean;
+}
