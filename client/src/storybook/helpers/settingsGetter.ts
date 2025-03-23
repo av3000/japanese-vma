cedb2f84c4d26cdf125183c0af331ddfd1167c09
av3000 <alanas.vaiciulis@gmail.com>
2025-03-23 18:58:39 +0100

    feat: add type fix and chromatic script

--- client/src/storybook/helpers/settingsGetter.ts
+++ client/src/storybook/helpers/settingsGetter.ts
@@ -1,53 +1,59 @@
-const getAllCssVars = (prefix: string | undefined = undefined, styleSheets = document.styleSheets): string[] => {
-	const cssVars: string[] = [];
+const getAllCssVars = (
+  prefix: string | undefined = undefined,
+  styleSheets = document.styleSheets
+): string[] => {
+  const cssVars: string[] = [];
 
-	for (let i = 0; i < styleSheets.length; i++) {
-		// try/catch used because 'hasOwnProperty' doesn't work
-		try {
-			for (let j = 0; j < styleSheets[i].cssRules.length; j++) {
-				try {
-					const cssRule = styleSheets[i].cssRules[j];
+  for (let i = 0; i < styleSheets.length; i++) {
+    // try/catch used because 'hasOwnProperty' doesn't work
+    try {
+      for (let j = 0; j < styleSheets[i].cssRules.length; j++) {
+        try {
+          const cssRule = styleSheets[i].cssRules[j];
 
-					if (!(cssRule instanceof CSSStyleRule)) {
-						continue;
-					}
+          if (!(cssRule instanceof CSSStyleRule)) {
+            continue;
+          }
 
-					// loop stylesheet's cssRules' style (property names)
-					for (let k = 0; k < cssRule.style.length; k++) {
-						const name = cssRule.style[k];
-						// test name for css variable signature, optional prefix and uniqueness
-						if (name.startsWith(`--${prefix || ''}`) && !cssVars.includes(name)) {
-							cssVars.push(name);
-						}
-					}
-				} catch {
-					/* empty */
-				}
-			}
-		} catch {
-			/* empty */
-		}
-	}
+          // loop stylesheet's cssRules' style (property names)
+          for (let k = 0; k < cssRule.style.length; k++) {
+            const name = cssRule.style[k];
+            // test name for css variable signature, optional prefix and uniqueness
+            if (
+              name.startsWith(`--${prefix || ""}`) &&
+              !cssVars.includes(name)
+            ) {
+              cssVars.push(name);
+            }
+          }
+        } catch {
+          /* empty */
+        }
+      }
+    } catch {
+      /* empty */
+    }
+  }
 
-	return cssVars;
+  return cssVars;
 };
 
 export const getElemCSSVars = (
-	prefix: string | undefined = undefined,
-	element = document.documentElement,
-	pseudo = undefined,
+  prefix: string | undefined = undefined,
+  element = document.documentElement,
+  pseudo = undefined
 ): Record<string, string> => {
-	const allCSSVars = getAllCssVars(prefix);
-	const elStyles = window.getComputedStyle(element, pseudo);
-	const cssVars = {};
+  const allCSSVars = getAllCssVars(prefix);
+  const elStyles = window.getComputedStyle(element, pseudo);
+  const cssVars: Record<string, string> = {};
 
-	for (let i = 0; i < allCSSVars.length; i++) {
-		const key = allCSSVars[i];
-		const value = elStyles.getPropertyValue(key);
+  for (let i = 0; i < allCSSVars.length; i++) {
+    const key = allCSSVars[i];
+    const value = elStyles.getPropertyValue(key);
 
-		if (value) {
-			cssVars[key] = value;
-		}
-	}
-	return cssVars;
+    if (value) {
+      cssVars[key] = value;
+    }
+  }
+  return cssVars;
 };
