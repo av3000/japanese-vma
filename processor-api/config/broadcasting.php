90c49650388deeff7e7ea302a599179c2d477343
av3000 <alanas.vaiciulis@gmail.com>
2025-12-30 22:59:46 +0100

    feat: Add redis containers, init broadcasting to prepare for websockets

--- processor-api/config/broadcasting.php
+++ processor-api/config/broadcasting.php
@@ -30,14 +30,20 @@ return [
 
     'connections' => [
 
-        'pusher' => [
-            'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY'),
-            'secret' => env('PUSHER_APP_SECRET'),
-            'app_id' => env('PUSHER_APP_ID'),
+        'reverb' => [
+            'driver' => 'reverb',
+            'host' => env('REVERB_HOST'),
+            'port' => env('REVERB_PORT'),
+            'scheme' => env('REVERB_SCHEME', 'https'),
+            'app_id' => env('REVERB_APP_ID'),
+            'key' => env('REVERB_APP_KEY'),
+            'secret' => env('REVERB_APP_SECRET'),
+            'debug' => env('APP_DEBUG', false),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
+                // If you were using a "cluster" in a previous Pusher setup, you might map it here,
+                // but for direct Reverb connection, it's often not explicitly needed unless for specific Reverb features.
+                'cluster' => env('REVERB_CLUSTER'),
+                'useTLS' => true
             ],
         ],
 
